36e0e5aec06930eca809577b13aaf5619d4ef325
test20150106 <noreply>
2015-01-06 18:25:06 +0000

    Update components

--- ui/hello-world-dom.reel/hello-world-dom.js
+++ ui/hello-world-dom.reel/hello-world-dom.js
@@ -13,5 +13,13 @@ exports.HelloWorldDom = Component.specialize(/** @lends HelloWorldDom# */ {
         value: function HelloWorldDom() {
             this.super();
         }
+    },
+
+    draw: {
+        value: function () {
+            var div = document.createElement("div");
+            div.textContent = "hello, world!";
+            this.element.appendChild(div);
+        }
     }
 });
